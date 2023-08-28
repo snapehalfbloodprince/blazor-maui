8dfef4c0ecbe6250d32008fcc28e6a2463227ae3
francesco <francesco.paolo.piga@avanade.com>
2023-08-28 15:27:27 +0200

    Creazione Componente UserList e prime prove

---
+++ UserRegistry.UI/Models/UserListItem.cs
@@ -0,0 +1,12 @@
+﻿namespace UserRegistry.UI.Models
+{
+	public class UserListItem
+	{
+		public int Id { get; set; }
+		public string? FirstName { get; set; }
+		public string? LastName { get; set; }
+		public string? Address { get; set; }
+		public DateTime CreatedAt { get; set; }
+	}
+}
+
